1c769a2dd561add60dce2addabcb803f81bb4645
Sara Dwayne Linde <lindesara@gmail.com>
2017-04-20 17:36:29 -0700

    Adds themes to CI application Here is stuff for the description field.

--- application/config/custom_config.php
+++ application/config/custom_config.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 //custom_config.php - a place to save global settings
 
-$config['style'] = 'amelia.css';
+$config['style'] = 'flatly.css';
 $config['banner'] = 'Default Banner';
 $config['title'] = 'Default Title';
 $config['copyright'] = 'Default Copyright';
