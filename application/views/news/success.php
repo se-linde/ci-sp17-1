f48acab1c11c947761982d7bf9ea877a7ce94737
Sara Dwayne Linde <lindesara@gmail.com>
2017-04-20 17:54:11 -0700

    final commit for this assignment.
    All 4 pages under
    /Users/Lugano/Desktop/school/GitHub/ci-sp17-1/application/views/news/
    now have the Bootstrap Theme applied to them.

--- application/views/news/success.php
+++ application/views/news/success.php
@@ -1,6 +1,15 @@
 <?php
 //application/views/news/success.php
+
+$this->load->view($this->config->item('theme') . 'header');
+
 ?>
 
 <p><b>Yay - you successfully created a news item! </b></p>
 <p>Now I suppose you want to see it? </p>
+
+<?php 
+
+$this->load->view($this->config->item('theme') . 'footer');  
+
+?> 
