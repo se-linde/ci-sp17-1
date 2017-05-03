c899aa32bc17ac2b070c8c3677312335804283b3
Sara Dwayne Linde <lindesara@gmail.com>
2017-05-02 18:35:39 -0700

    Removed dev folder.
    .

--- dev/050217/views/pages/home.php
+++
@@ -1,1 +0,0 @@
-<h2>This is the Home Page.</h2>
--- dev/050217/views/templates/footer.php
+++
@@ -1,7 +0,0 @@
-<?php 
-// application/views/templates/footer.php
-?> 
-
-                <em>&copy; 2015 ~ Dwayne Linde</em>
-        </body>
-</html>
